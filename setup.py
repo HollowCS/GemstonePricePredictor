dce08f71f45d14b5d8c55d0ec7f2abb4b50f65dc
theAFKhero <madhusudan74748@outlook.com>
2023-06-25 18:11:16 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+HYPEN_E_DOT = '-e .'
+
+
+def get_requirements(file: str)->list[str]:
+    required_file = []
+    with open(file) as file_obj:
+        required_file = file_obj.readlines()
+        [req.replace("\n", "") for req in required_file]
+
+        if HYPEN_E_DOT in required_file:
+            required_file.remove(HYPEN_E_DOT)
+    return required_file
+
+setup(
+    name="Regression Project",
+    version="0.0.1",
+    author="Madhu sudan Mandal",
+    author_email="madhsudan78648@gmail.com",
+    install_requires=get_requirements("requirements.txt"),
+    packages=find_packages()
+
+)
